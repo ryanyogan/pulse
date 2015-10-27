3bd6a84e849be346793b89f05ddb7db06c53ad02
Ryan Yogan <ryanyogan@gmail.com>
2015-10-27 13:54:02 -0400

    added a header

---
+++ universal/components/Header.js
@@ -0,0 +1,13 @@
+import React, { PropTypes, Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header className='Pulse-header'>
+          <h1>Pulse</h1>
+        </header>
+      </div>
+    );
+  }
+}
